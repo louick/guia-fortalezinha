5f3082327443df7211c1c3ee74674d2a2d0c5863
louick <louickmytc@gmail.com>
2025-04-10 19:01:19 -0300

    Ajustes nas acomodações e atualização da galeria; nova página Restaurantes na Praia; configuração para GitHub Pages

--- src/pages/Galeria.js
+++ src/pages/Galeria.js
@@ -10,24 +10,24 @@ import { ZoomInOutlined } from '@ant-design/icons';
 const { Content } = Layout;
 const { Title } = Typography;
 
-// O primeiro item usa uma imagem da pasta public
+// Atualização: utilizando process.env.PUBLIC_URL para referenciar as imagens na pasta public.
 const data = [
-  { id: 1, image: '/galeria1.jpg' },
-  { id: 2, image: '/galeria1.jpeg' },
-  { id: 3, image: '/galeria3.jpg' },
-  { id: 4, image: '/galeria4.jpg' },
-  { id: 5, image: '/galeria5.jpg' },
-  { id: 6, image: '/galeria6.jpg' },
-  { id: 7, image: '/galeria7.jpg' },
-  { id: 8, image: '/galeria8.jpg' },
-  { id: 9, image: '/galeria9.jpg' },
-  { id: 10, image: '/galeria10.jpg' }
+  { id: 1, image: process.env.PUBLIC_URL + '/galeria1.jpg' },
+  { id: 2, image: process.env.PUBLIC_URL + '/galeria1.jpeg' },
+  { id: 3, image: process.env.PUBLIC_URL + '/galeria3.jpg' },
+  { id: 4, image: process.env.PUBLIC_URL + '/galeria4.jpg' },
+  { id: 5, image: process.env.PUBLIC_URL + '/galeria5.jpg' },
+  { id: 6, image: process.env.PUBLIC_URL + '/galeria6.jpg' },
+  { id: 7, image: process.env.PUBLIC_URL + '/galeria7.jpg' },
+  { id: 8, image: process.env.PUBLIC_URL + '/galeria8.jpg' },
+  { id: 9, image: process.env.PUBLIC_URL + '/galeria9.jpg' },
+  { id: 10, image: process.env.PUBLIC_URL + '/galeria10.jpg' }
 ];
 
 function Galeria() {
   const [lightboxOpen, setLightboxOpen] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
-  
+
   // Cria um array de slides com as URLs das imagens
   const slides = data.map(item => ({ src: item.image }));
 
